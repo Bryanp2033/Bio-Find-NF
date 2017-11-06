3ee4d7a60894c8589a5f4263d798e165265d4def
Bryan <bryanporras2033@gmail.com>
2017-11-06 11:09:14 -0500

    working with login

--- controllers/controller.js
+++ controllers/controller.js
@@ -3,9 +3,23 @@ var express = require("express")
 var router = express.Router();
 var user = require("../models/user.js");
 
-router.get("/", function(request, response){
-    response.render("index", {layout: 'main'})
+router.get("/", function(req, res){
+    res.render("index", {layout: 'main'})
     console.log("Working Login")
 })
+
+
+router.get("/login", function(req, res){
+    console.log(req)
+})
+
+router.get('/signup', function(req, res){
+    console.log(res);
+})
+
+router.get("/guest", function(req, res){
+    console.log("hello")
+})
+
 
 module.exports = router;
